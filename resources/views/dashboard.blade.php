72549cbc9f5e759f2fc0401cd2fa405fb903c513
abidarkan <obetsiak26@gmail.com>
2024-11-23 17:22:54 +0700

    Change a bit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
               <div class="d-flex flex-column h-100">
                 <p class="mb-1 pt-2 text-bold">Pengumuman terbaru</p>
                 <h5 class="font-weight-bolder">(Judul)</h5>
-                <p class="mb-5">(isi Pengumuman)</p>
+                <p class="mb-5">(isi asdasd)</p>
                 <a class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="javascript:;">
                   Read More
                   <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
